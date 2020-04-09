cbda789e20ec676e46e4dc03b3aa5626cb259d1e
charles <48330531+ulookme@users.noreply.github.com>
2020-04-09 15:39:51 +0200

    erreur.sys

--- projet_2/titanic/Untitled.R
+++
@@ -1,3 +0,0 @@
-file_avantage <- ""
-data_avantage <- read.table(file_avantage, header = TRUE, sep = ";", quote = "\"",dec = ",", fill = TRUE, comment.char = "", encoding = "utf-8")
-data_avantage <- data.frame(data_avantage)
